f13ab105dd4fcc2e39e5eb2f592d865b18c03a45
Leigh MacDonald <leigh.macdonald@gmail.com>
2015-10-20 17:35:49 -0600

    BP.tf currency endpoint fully decoded

--- backpack.go
+++ backpack.go
@@ -1,1 +1,48 @@
 package twirc
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type (
+	BPCurrencyResponse struct {
+		Response BPCurrencyBase `json:"response"`
+	}
+
+	BPCurrencyBase struct {
+		Success    int                            `json:"success"`
+		Currencies map[string]BPCurrencyContainer `json:"currencies"`
+		Name       string                         `json:"name"`
+		URL        string                         `json:"url"`
+	}
+
+	BPCurrencyContainer struct {
+		Quality    int        `json:"quality"`
+		PriceIndex int        `json:"priceindex"`
+		Single     string     `json:"single"`
+		Plural     string     `json:"plural"`
+		Round      int        `json:"round"`
+		Blanket    int        `json:"blanket"`
+		Craftable  string     `json:"craftable"`
+		Tradable   string     `json:"tradable"`
+		DefIndex   int        `json:"defindex"`
+		Price      BPCurrency `json:"price"`
+	}
+
+	BPCurrency struct {
+		Value      float64 `json:"value"`
+		ValueHigh  float64 `json:"Value_high"`
+		Currency   string  `json:"Currebct"`
+		Difference float64 `json:""Difference`
+	}
+)
+
+func decodeBPCurrencyResponse(resp_body []byte, resp *BPCurrencyResponse) error {
+	err := json.Unmarshal(resp_body, resp)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
--- backpack_test.go
+++ backpack_test.go
@@ -1,1 +1,23 @@
 package twirc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestDecodeBPCurrencyResponse(t *testing.T) {
+	var resp BPCurrencyResponse
+	json_data, err := ioutil.ReadFile("./fixtures/bp_tf_currencies.json")
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	err = decodeBPCurrencyResponse(json_data, &resp)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	res2B, _ := json.Marshal(resp)
+	fmt.Println(string(res2B))
+}
